0ea4a63716971bb797d8012fde6e2069dc2ae4fa
kindmemento <kindmemento@gmail.com>
2024-03-25 10:00:37 +0300

    implement static classes for running examples making use of singly and doubly linked list data structures, call them sequentially in main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import DoublyLinkedList.DoublyLinkedList;
+import SinglyLinkedList.SinglyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        runSinglyLinkedList();
+//        runDoublyLinkedList();
+    }
+
+    static void runSinglyLinkedList() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.first().next.value);
+        System.out.println("Current Length of List: " + list.getLength());
+        System.out.println(list.at(1).value);
+        list.remove();
+        System.out.println(list.last().value);
+        System.out.println("Current Length of List: " + list.getLength());
+    }
+
+    static void runDoublyLinkedList() {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(3);
+        dll.add(2);
+        dll.add(1);
+        System.out.println(dll.first().value);
+        System.out.println("Current Length of Doubly LinkedList: " + dll.getLength());
     }
 }

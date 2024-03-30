1a64f8d42a8259d0c370edb22c12c1cd343b889e
kindmemento <kindmemento@gmail.com>
2024-03-30 22:24:07 +0300

    copy implemented singly linked list data structure to midterm package

---
+++ src/midterm/SinglyLinkedList/ListNode.java
@@ -0,0 +1,15 @@
+package midterm.SinglyLinkedList;
+
+public class ListNode<V> {
+    public V value;
+    public ListNode<V> next;
+
+    public ListNode(V value, ListNode<V> next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public V value() { return value; }
+    public boolean hasNext() { return next != null; }
+    public ListNode<V> next() { return next; }
+}

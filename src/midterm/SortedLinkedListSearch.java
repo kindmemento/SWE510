eef2e818055bc04e6db4f0b3c60d0faaa13d5307
kindmemento <kindmemento@gmail.com>
2024-03-30 22:41:48 +0300

    implement binarySearch method

--- src/midterm/SortedLinkedListSearch.java
+++ src/midterm/SortedLinkedListSearch.java
@@ -1,14 +1,34 @@
 package midterm;
 
-import java.util.LinkedList;
+import midterm.SinglyLinkedList.SinglyLinkedList;
 
 public class SortedLinkedListSearch {
     public static void main(String[] args) {
         // @TODO: Implement main method
     }
 
-    public static boolean binarySearch(LinkedList<Integer> list, int target) {
-        // @TODO: Implement binarySearch method
-        return false; // placeholder
+    public static boolean binarySearch(SinglyLinkedList<Integer> list, int target) {
+        int left = 0;
+        int right = list.getLength() - 1;
+        int traversedNodes = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = list.get(mid);
+
+            if (midValue == target) {
+                System.out.println("Number of traversed nodes: " + (traversedNodes + 1));
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+            traversedNodes++;
+        }
+
+        System.out.println("Number of traversed nodes: " + traversedNodes);
+        return false;
     }
 }

4139ce69b4339202fcaa1563ee5adba96017e85a
kindmemento <kindmemento@gmail.com>
2024-03-30 22:18:34 +0300

    implement boilerplate for sorted linked list search public class with stubbed class methods main and binarySearch

---
+++ src/midterm/SortedLinkedListSearch.java
@@ -0,0 +1,14 @@
+package midterm;
+
+import java.util.LinkedList;
+
+public class SortedLinkedListSearch {
+    public static void main(String[] args) {
+        // @TODO: Implement main method
+    }
+
+    public static boolean binarySearch(LinkedList<Integer> list, int target) {
+        // @TODO: Implement binarySearch method
+        return false; // placeholder
+    }
+}
